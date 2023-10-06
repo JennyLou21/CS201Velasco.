26ed67d8bb62be5185b602899d9a091cb786ef0c
JennyLou21 <147123156+JennyLou21@users.noreply.github.com>
2023-10-06 11:48:05 +0800

    Add files via upload
    BSCS 21
    Velasco, Jenny Lou

---
+++ DSA Act 1/act1.js
@@ -0,0 +1,11 @@
+let FirstFavNum = 1;
+let SecondFavNum = 10;
+console.log(FirstFavNum + SecondFavNum);
+console.log(FirstFavNum - SecondFavNum);
+console.log(FirstFavNum / SecondFavNum);
+console.log(FirstFavNum * SecondFavNum);
+console.log(FirstFavNum % SecondFavNum);
+
+let Power = Math.pow(FirstFavNum);
+console.log(Power);
+
