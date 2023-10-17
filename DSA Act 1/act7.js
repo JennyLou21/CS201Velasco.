02fa9e4ba0f076ebcb84180b5c41924ca40c3db5
JennyLou21 <147123156+JennyLou21@users.noreply.github.com>
2023-10-17 15:25:27 +0800

    Add files via upload

---
+++ DSA Act 1/act7.js
@@ -0,0 +1,8 @@
+let pword = 'p@sswOrd@2023';
+
+if(pword.length >= pword.includes("p@ssword")){
+    console.log("valid password");
+}
+else{
+    console.log("Invalid password");
+}
---
+++ DSA Act 1/act8.js
@@ -0,0 +1,17 @@
+const favSingers =[ 'Taylor Swift', 'Ben and Ben', 'Morisette Amon' ];
+
+console.log ( favSingers [0]);
+console.log ( favSingers [1]);
+console.log ( favSingers [2]);
+
+
+const favNumbers = [ '10', '1', '20', '21'];
+
+console.log ( favNumbers [0]);
+console.log ( favNumbers [1]);
+console.log ( favNumbers [2]);
+console.log ( favNumbers [3]);
+
+const mixedArr = [ " string ", ["otherarray"], 123, true];
+
+console.log ( favSingers [0], favSingers [1], favSingers[2], favNumbers [1], mixedArr [3]);
